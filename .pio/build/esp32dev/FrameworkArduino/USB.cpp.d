c7f7f6849e8d7734e69cc072edd359a6bbb9788d
Machinoro <95751244+machinoro@users.noreply.github.com>
2026-01-25 11:32:39 +0700

    fix: remove .pio folder from tracking

--- .pio/build/esp32dev/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/USB.cpp.o: \
- /home/duck/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- /home/duck/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/duck/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32dev/FrameworkArduino/USBCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/USBCDC.cpp.o: \
- /home/duck/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
- /home/duck/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/duck/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
